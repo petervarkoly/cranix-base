d2b24873c622d1eecced2d24a027e7bf1caa8533
Dipl. Ing. Péter Varkoly <pvarkoly@cephalix.eu>
2021-10-18 13:50:08 +0200

    Merge the 2 samba configurations.

--- tools/merge-registry.py
+++ tools/merge-registry.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
+import configparser
 import os
 
 #{
@@ -7,6 +7,33 @@ import os
 #data(4) = "\00\00\00\00"
 #}
 
+#Add printserver settings to admins smb.conf
+config = configparser.ConfigParser(delimiters=('='))
+config.read('/etc/samba/smb.conf')
+config.set('global','printing','CUPS')
+config.set('global','rpc_server:spoolss','external')
+config.set('global','rpc_daemon:spoolssd','fork')
+config.add_section('print$')
+config.set('print$','browseable','yes')
+config.set('print$','comment','Printer Drivers')
+config.set('print$','create mask','0664')
+config.set('print$','directory mask','0775')
+config.set('print$','force group','SYSADMINS')
+config.set('print$','guest ok','no')
+config.set('print$','path','/var/lib/samba/drivers')
+config.set('print$','printable','no')
+config.set('print$','write list','@SYSADMINS root')
+config.set('print$','read only','no')
+config.add_section('printers')
+config.set('printers','browseable','yes')
+config.set('printers','comment','All Printers')
+config.set('printers','create mask','0600')
+config.set('printers','path','/var/tmp')
+config.set('printers','printable','yes')
+with open('/etc/samba/smb.conf','wt') as f:
+    config.write(f)
+
+#Merge the registries
 registry={}
 key=""
 for line in os.popen("tdbdump /var/lib/samba/registry.tdb").readlines():
