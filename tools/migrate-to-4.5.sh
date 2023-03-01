e0828360a91664ecdc0310e71c58ad3ef0fd1e58
Dipl. Ing. Péter Varkoly <pvarkoly@cephalix.eu>
2023-03-01 08:13:31 +0100

    samba-ad stands

--- tools/migrate-to-4.5.sh
+++ tools/migrate-to-4.5.sh
@@ -4,7 +4,6 @@ DATE=$( /usr/share/cranix/tools/crx_date.sh )
 sed -i s#CRANIX/4.4#CRANIX/4.5# /etc/zypp/credentials.cat
 sed -i s/4.4/4.5/ /etc/zypp/repos.d/CRANIX.repo
 /usr/bin/zypper ref
-sed -i "s/samba-ad /samba-ad-dc /" /etc/sysconfig/cranix
 #To be safe if samba will not start after update.
 sed -i /repo1.cephalix.eu/d /etc/hosts
 host repo1.cephalix.eu | gawk '{ print $4 " repo1.cephalix.eu vpn.cephalix.eu" }' >> /etc/hosts
@@ -26,7 +25,7 @@ mysqldump --databases CRX > /var/adm/cranix/backup/${DATE}/CRX.sql
 /usr/bin/zypper -n dup 2>&1 | tee /var/log/CRANIX-MIGRATE-TO-4-5
 
 if [ "$( rpm -q --qf %{VERSION} cranix-base )" = "4.5" ]; then
-	/usr/bin/systemctl enable samba-ad-dc
+	/usr/bin/systemctl enable samba-ad
 	[ -e /etc/samba/smb-printserver.conf ] && /usr/bin/systemctl enable samba-printserver
 	[ -e /etc/samba/smb-fileserver.conf ]  && /usr/bin/systemctl enable samba-fileserver
 	if [ -e /usr/lib/systemd/system/cyrus-imapd.service ]; then
