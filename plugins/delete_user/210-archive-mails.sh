2f44d97af27a6b50d5ac241fa6c5273dc4e2aa23
Dipl. Ing. Péter Varkoly <pvarkoly@cephalix.eu>
2025-10-27 12:23:52 +0100

    New plugin to archive mail boxes. This script works only if the mailboxes are placed in /var/spool/dovecot

---
+++ plugins/delete_user/210-archive-mails.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#
+# Copyright (c) 2025 Peter Varkoly Nürnberg, Germany.  All rights reserved.
+#
+if [ ! -e /etc/sysconfig/cranix ]; then
+   echo "ERROR This ist not an CRANIX."
+   exit 1
+fi
+
+uid=''
+
+while read a
+do
+  b=${a/:*/}
+  if [ "$a" != "${b}:" ]; then
+     c=${a/$b: /}
+  else
+     c=""
+  fi
+  case "${b,,}" in
+    uid)
+      uid="${c}"
+    ;;
+  esac
+done
+
+if [ -z "$uid" ]; then
+   echo "ERROR You have to define an uid."
+   exit 4;
+fi
+
+MAILDIR="/var/spool/dovecot/${uid}/"
+CANDIR=$( readlink -e ${MAILDIR} )
+DATUM=$( /usr/share/cranix/tools/crx_date.sh )
+
+if [ -d "$MAILDIR" -a ${CANDIR} != "/var/spool/dovecot/" ];
+then
+    tar czf /home/archiv/${uid}-mails-${DATUM}.tgz ${MAILDIR} && rm -rf ${MAILDIR}
+fi
